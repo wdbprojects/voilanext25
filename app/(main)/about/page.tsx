f7a4ed5884c848b953f8a42c7fbad3f53104202c
MAC <ronyortiz@MACs-iMac.local>
2024-12-06 21:29:27 -0400

    Fix a typo

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -4,7 +4,7 @@ const AboutPage = async () => {
       <h1 className="flex flex-wrap items-center justify-center gap-2 text-center text-3xl font-medium leading-none tracking-wide sm:gap-x-6 sm:text-5xl">
         We love
         <span className="tracking-lg rounded-lg bg-primary px-4 py-2 text-white">
-          store
+          Voila!
         </span>
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 tracking-wide text-muted-foreground">
